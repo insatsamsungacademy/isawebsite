38e2c82fdd413bcd91adaae8178611c85a45bba1
NAJJARFatii <fatmanajjar119@gmail.com>
2017-09-18 00:29:29 +0200

    Faza o5ra
    faza o5ra

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
- try {
-         
-            $_bdd = new PDO("mysql:host=localhost;dbname=isa",'root','admin123');
-        } catch (PDOException $e) {
-            die('Erreur : ' . $e->getMessage());
-        }
-?>
